8d3890e7179b2e6bdd85aafcc21a4485b6495bf6
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2022-12-01 16:17:51 -0500

    Finished up. Need README and video walkthrough.

--- models/index.js
+++ models/index.js
@@ -16,7 +16,7 @@ Product.belongsToMany(Tag, {
 }),
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product, {
   through: ProductTag
 }),
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,7 +16,7 @@ router.get('/:id', (req, res) => {
   Category.findOne(
     {
       where: {
-        id: req.params.id
+        id: req.params.id,
       },
     }
   ).then((categoryData) => {
@@ -27,14 +27,48 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name,
+  })
+  .then((newCategory) => {
+    res.json(newCategory);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      id: req.body.id,
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  .then((updatedCategory) => {
+    res.json(updatedCategory);
+  })
+  .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((deletedCategory) => {
+    res.json(deletedCategory);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
